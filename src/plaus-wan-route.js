8aa3e7513a3999fceae239e4cfc7b52270b3b7cc
Drew Schuster <dschuster@slack-corp.com>
2018-11-30 11:06:08 -0800

    Testing

--- src/plaus-wan-route.js
+++ src/plaus-wan-route.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+require('isomorphic-fetch');
+
+const { WebClient } = require('@slack/client');
+
+const token = process.env.PLAUS_SLACK_BOT_USER_OAUTH_ACCESS_TOKEN || '';
+const web = new WebClient(token);
 
 function addPlausWanRoute(app) {
 	app.post('/plaus', (req, res) => {
@@ -6,9 +12,27 @@ function addPlausWanRoute(app) {
 		const { type, challenge } = reqBody;
 		if (type === 'url_verification') {
 			res.json({ challenge });
+		} else if (type === 'app_mention') {
+			const { event } = reqBody;
+			const { ts, channel } = event;
+			sendMessage(`testing ${ts}`, channel);
 		}
 		res.status(200).end();
 	});
+}
+
+function sendMessage(text, channel) {
+	return web.chat
+		.postMessage({
+			channel,
+			text,
+		})
+		.then(() => {
+			console.log('Message sent: ', text);
+		})
+		.catch(err => {
+			console.log('Error:', err);
+		});
 }
 
 module.exports = addPlausWanRoute;
