ed13a24f5c7ed9681f3f9772a7bd30679e5fec19
Drew Schuster <dschuster@slack-corp.com>
2022-02-06 18:00:26 -0800

    add support for daily tasks

---
+++ src/do-tasks.js
@@ -0,0 +1,107 @@
+require('dotenv').config();
+const childProcess = require('child_process');
+const path = require('path');
+const { WebClient } = require('@slack/client');
+
+const token = process.env.SLACK_API_TOKEN || '';
+const web = new WebClient(token);
+
+let someFailed = false;
+
+const logs = [[]];
+
+function log(message) {
+	const currentLogGroup = logs[logs.length - 1];
+	console.log(message);
+	currentLogGroup.push(message);
+}
+
+function newLogGroup() {
+	logs.push([]);
+}
+
+async function doTasks(tasks) {
+	tasks.forEach((task) => {
+		log(task);
+		const { status, stderr, stdout } = childProcess.spawnSync(
+			path.resolve(__dirname, `../bin/${task}`)
+		);
+
+		if (status) {
+			someFailed = true;
+			log(`Error executing \`${task}\`! Error: ${status}
+\`\`\`
+${stderr}
+\`\`\`
+`);
+		} else {
+			if (stderr.length) {
+				someFailed = true;
+				log(`Error Output:
+\`\`\`
+${stderr.toString().trim()}
+\`\`\`
+`);
+			}
+			if (stdout.length) {
+				log(`\`\`\`
+${stdout.toString().trim()}
+\`\`\`
+`);
+			}
+		}
+		log(`${status || stderr.length ? '❌' : '✅'}`);
+		newLogGroup();
+	});
+
+	return web.chat
+		.postMessage({
+			channel: process.env.CRON_LOGS_CHANNEL_ID,
+			text: someFailed
+				? '❌: At least one hourly task failed! @drew 🧵'
+				: '✅ Hourly Tasks completed successfully!',
+			link_names: true,
+		})
+		.then(({ ts }) => {
+			const blocks = [];
+			logs.forEach((logGroup) => {
+				if (!logGroup.length) return;
+				const title = logGroup.shift();
+				const result = logGroup.pop();
+				blocks.push({
+					type: 'header',
+					text: { type: 'plain_text', text: `${result} ${title}`, emoji: true },
+				});
+				blocks.push({
+					type: 'section',
+					text: { type: 'mrkdwn', text: logGroup.join('\n') },
+				});
+				blocks.push({ type: 'divider' });
+			});
+
+			// Remove the trailing divider
+			blocks.pop();
+
+			return web.chat
+				.postMessage({
+					channel: process.env.CRON_LOGS_CHANNEL_ID,
+					blocks,
+					thread_ts: ts,
+					// link_names: true,
+				})
+				.then(() => {
+					console.log('Logs sent to Slack');
+					process.exit(0);
+				})
+				.catch((err) => {
+					console.log('Failed to send logs to Slack:', err);
+					process.exit(1);
+				});
+		})
+		.catch((err) => {
+			console.log('Failed to send logs to Slack:', err);
+			process.exit(1);
+		});
+}
+
+module.exports = doTasks;
