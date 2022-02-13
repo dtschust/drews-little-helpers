66bdce49f28aaca0c6da54e8cf3e5f88de4b8002
Drew Schuster <dschuster@slack-corp.com>
2022-02-13 11:42:54 -0800

    Switch to fetch for getting ptp cookie, one less use of request

--- src/utils/get-ptp-login-cookie.js
+++ src/utils/get-ptp-login-cookie.js
@@ -1,4 +1,6 @@
-const request = require('request');
+require('dotenv').config();
+require('isomorphic-fetch');
+const FormData = require('form-data');
 const mongoose = require('mongoose');
 const PtpCookie = require('../mongoose-models/Ptp-Cookie');
 
@@ -9,36 +11,35 @@ mongoose.connect(process.env.MONGO_DB_URI, {
 });
 
 function getPtpLoginCookies() {
-	return new Promise((resolve, reject) => {
-		request.post(
-			'https://passthepopcorn.me/ajax.php?action=login',
-			{
-				form: {
-					username: process.env.PTP_USERNAME,
-					password: process.env.PTP_PASSWORD,
-					passkey: process.env.PTP_PASSKEY,
-					// WhatsYourSecret:
-					// 	'Hacker! Do you really have nothing better do than this?',
-					keeplogged: 1,
-				},
-			},
-			(error, response) => {
-				if (response.statusCode === 403) {
-					console.log('Looks I got captcha-ed, giving up!');
-					reject(new Error('Captcha-ed'));
-				}
+	const formData = new FormData();
+	formData.append('username', process.env.PTP_USERNAME);
+	formData.append('password', process.env.PTP_PASSWORD);
+	formData.append('passkey', process.env.PTP_PASSKEY);
+	// formData.append('WhatsYourSecret', 'Hacker! Do you really have nothing better do than this?');
+	formData.append('keeplogged', 1);
 
-				const cookies = response.headers['set-cookie'];
-				const persistedCookie = cookies.map((cookie) => cookie.split(';')[0]).join(';');
-				const cookieToPersist = new PtpCookie({ cookie: persistedCookie });
-				PtpCookie.deleteMany(undefined).then(() => {
-					cookieToPersist.save().then(() => {
-						resolve(persistedCookie);
-						// resolve(cookies);
-					});
-				});
-			}
-		);
+	return fetch('https://passthepopcorn.me/ajax.php?action=login', {
+		method: 'POST',
+		body: formData,
+	}).then(async (resp) => {
+		const { status } = resp;
+		if (status === 403) {
+			console.log('Looks I got captcha-ed, giving up!');
+			return new Error('Captcha-ed');
+		}
+
+		const cookies = resp.headers.get('set-cookie');
+		// The goal:
+		// PHPSESSID=asdfasdf;session=asdfasdf
+		const persistedCookie = cookies
+			.split(' path=/; secure; HttpOnly, ')
+			.map((c) => c.split(';')[0])
+			.join(';');
+
+		const cookieToPersist = new PtpCookie({ cookie: persistedCookie });
+		await PtpCookie.deleteMany(undefined);
+		await cookieToPersist.save();
+		return persistedCookie;
 	});
 }
 
