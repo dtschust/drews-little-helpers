16fb8104349cc97be4c8927d557468dbbf15cb4b
Drew Schuster <dtschust@gmail.com>
2022-12-25 23:35:12 -0800

    fix POSTs and DELETEs

--- src/fb-proxy-route.js
+++ src/fb-proxy-route.js
@@ -2,6 +2,18 @@ const httpProxy = require('http-proxy');
 
 const apiProxy = httpProxy.createProxyServer({ secure: false });
 
+// https://github.com/http-party/node-http-proxy/issues/1142#issuecomment-282810543
+apiProxy.on('proxyReq', (proxyReq, req) => {
+	if (req.body) {
+		const bodyData = JSON.stringify(req.body);
+		// incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+		proxyReq.setHeader('Content-Type', 'application/json');
+		proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+		// stream the content
+		proxyReq.write(bodyData);
+	}
+});
+
 function enableCors(req, res) {
 	if (req.headers['access-control-request-method']) {
 		res.setHeader('access-control-allow-methods', req.headers['access-control-request-method']);
