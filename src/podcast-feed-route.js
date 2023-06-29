d52ed983618c5bd21185a23e98be4f9a537a5be8
Drew Schuster <dtschust@gmail.com>
2023-06-28 22:42:40 -0700

    hacky index page

--- src/podcast-feed-route.js
+++ src/podcast-feed-route.js
@@ -2,6 +2,29 @@ const path = require('path');
 const fs = require('fs').promises;
 
 function addPodcastFeedRoute(app) {
+	app.get('/feeds/', async (req, res) => {
+		try {
+			res.status(200)
+				.send(
+					`
+<html><head></head><body>
+<ul>
+	<li><a href="161410.xml">Peecast Blast</a></li>
+	<li><a href="1001514.xml">Neighborhood Listen</a></li>
+	<li><a href="1001522.xml">Big Grande Teacher's Lounge</a></li>
+	<li><a href="1001557.xml">Andy Daly Podcast Pilot Project</a></li>
+	<li><a href="1001575.xml">I Was There Too</a></li>
+	<li><a href="1001823.xml">Celebrity Sighting! With Jonathan Biting!</a></li>
+	<li><a href="474009.xml">The Gino Lombardo Show</a></li>
+</ul>
+</body></html>`
+				)
+				.end();
+		} catch (e) {
+			console.log(e);
+			res.status(500).end();
+		}
+	});
 	app.get('/feeds/:feedId', async (req, res) => {
 		try {
 			const feedId = parseInt(req?.params?.feedId.match(/\d+/), 10);
