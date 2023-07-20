b2180f5fd7c0c0c2ae6a3fa437e3e1353c8678ad
Drew Schuster <dtschust@gmail.com>
2023-07-19 23:31:38 -0700

    server controls update timestamp

--- src/frame-controller-status-route.js
+++ src/frame-controller-status-route.js
@@ -5,7 +5,10 @@ const FrameControllerStatus = require('./mongoose-models/Frame-Controller-Status
 function addFrameControllerStatusRoute(app) {
 	app.post('/frameControllerStatus', async (req, res) => {
 		const reqBody = req.body;
-		const frameControllerStatusModel = new FrameControllerStatus(reqBody);
+		const frameControllerStatusModel = new FrameControllerStatus({
+			...reqBody,
+			time: Date.now(),
+		});
 		await FrameControllerStatus.deleteMany();
 		await frameControllerStatusModel.save();
 		res.status(200).end();
--- src/frame-status-route.js
+++ src/frame-status-route.js
@@ -5,7 +5,7 @@ const FrameStatus = require('./mongoose-models/Frame-Status');
 function addFrameStatusRoute(app) {
 	app.post('/frameStatus', async (req, res) => {
 		const reqBody = req.body;
-		const frameStatusModel = new FrameStatus(reqBody);
+		const frameStatusModel = new FrameStatus({ ...reqBody, time: Date.now() });
 		await FrameStatus.deleteMany();
 		await frameStatusModel.save();
 		res.status(200).end();
